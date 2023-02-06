aec16768561f6ebc2f2742bf5b8c7bc7195fc8fc
Dileepa Mabulage <Dileepa.Mabulage@inivossl.com>
2023-02-06 16:44:29 +0530

    Add project files.

---
+++ testAwsConsoleApp/AWSCloudWatch.cs
@@ -0,0 +1,71 @@
+﻿using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace testAwsConsoleApp
+{
+    public class AWSCloudWatch
+    {
+        private readonly IConfiguration _configuration;
+        private static System.Timers.Timer aTimer;
+
+        public AWSCloudWatch(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void TimerStart()
+        {
+            Console.WriteLine("\nPress the Enter key to exit the application...\n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            aTimer = new System.Timers.Timer(4000); // method executes every 4 seconds
+            aTimer.Elapsed += (s, e) => CloudWatchLog();
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            Console.ReadLine();
+            aTimer.Stop();
+            aTimer.Dispose();
+            Console.WriteLine("Terminating the application...");
+
+            while (Console.Read() != 'q') ;
+
+        }
+
+        public async void CloudWatchLog()
+        {
+            try
+            {
+                var credentials = new Amazon.Runtime.BasicAWSCredentials(_configuration["Client_id"], _configuration["Client_secret"]);
+
+                var config = new AmazonCloudWatchLogsConfig
+                {
+                    RegionEndpoint = Amazon.RegionEndpoint.APSoutheast1
+                };
+
+                var logClient = new AmazonCloudWatchLogsClient(credentials, config);
+
+                await logClient.PutLogEventsAsync(new PutLogEventsRequest()
+                {
+                    LogGroupName = _configuration["LogGroupName"],
+                    LogStreamName = _configuration["LogStreamName"],
+                    LogEvents = new List<InputLogEvent>()
+                    {
+                        new InputLogEvent()
+                        {
+                            Message = "error message from console app",
+                            Timestamp = DateTime.UtcNow
+                        }
+                    }
+
+                });
+                Console.WriteLine("Logging successfull");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message, "Error occured");
+            }
+
+        }
+    }
+}
---
+++ testAwsConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace testAwsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            IConfiguration configuration = configBuilder.Build();
+
+            var cloudwatchParam = new AWSCloudWatch(configuration);
+
+            cloudwatchParam.TimerStart();
+        }
+
+    }
+}
+
+
